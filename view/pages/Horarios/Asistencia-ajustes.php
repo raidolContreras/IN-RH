a6a0e444d339049bfcf4f5a8f2e527c4bbd81cb9
raidolContreras <oscarcontrerasf91@gmail.com>
2023-05-26 16:24:55 -0600

    implementación de ajustes de horarios

---
+++ view/pages/Horarios/Asistencia-ajustes.php
@@ -0,0 +1,17 @@
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<div class="container-fluid dashboard-content">
+  <div class="ecommerce-widget">
+    <div class="card mx-5">
+      <div class="card-header encabezado">Configuración del registro de horas</div>
+      <div class="row">
+        <div class="card-side-nav col-2 lista-ajustes">
+          <div class=""><a href="">Horarios de trabajo</a></div>
+          <div class=""><a href="">Importar horarios</a></div>
+          <div class=""><a href="">Exportar resultados</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
