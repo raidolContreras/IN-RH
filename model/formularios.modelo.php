2d66d88cd0bbb311817dfe0dbbbea00912d44944
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-16 15:24:37 -0600

    Update formularios.modelo.php

--- model/formularios.modelo.php
+++ model/formularios.modelo.php
@@ -714,7 +714,7 @@ class ModeloFormularios{
 	static public function mdlVerPostulantes($tabla, $item, $valor){
 		if ($item == null && $valor == null) {
 			$sql = "SELECT * FROM $tabla 
-			JOIN Vacantes ON $tabla.Vacantes_idVacantes = Vacantes.idVacantes 
+			JOIN vacantes ON $tabla.Vacantes_idVacantes = Vacantes.idVacantes 
 			WHERE $tabla.statusPostulante = 1;";
 			$stmt = Conexion::conectar()->prepare($sql);
 			$stmt->execute();
